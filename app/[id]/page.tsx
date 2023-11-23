d42a1dd88f1b0d8661998d29aee8e6116f8a3341
Dhany Dwi Putra <dhany.dwi@nabatisnack.co.id>
2023-11-23 17:10:15 +0700

    new thme

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -1,10 +1,9 @@
+import SmoothTheme from '../themes/Smooth';
 function Page({params}: any) {
 	const { id } = params; 
 	return (
 		<>
-			<div className="container mx-auto">
-				<div className="bg-yellow-200 w-full h-[200px]">Box {id}</div>
-			</div>
+			<SmoothTheme />
 		</>
 	);
 }
